b63bf4f1900fe9ade5a2f47db8619803f69f1bfc
diskostu <denny.kluge@gmail.com>
2022-02-19 22:31:31 +0100

    some initial demos

---
+++ src/demo/demo1/Demo1.java
@@ -0,0 +1,40 @@
+package demo1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@SpringBootApplication
+@Slf4j
+public class Demo1 {
+
+    public static void main(String[] args) {
+        final ConfigurableApplicationContext context = SpringApplication.run(Demo1.class, args);
+    }
+}
+
+
+@Component
+@Slf4j
+class DateAtStartTime implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) {
+        System.out.println(LocalDateTime.now());
+
+        System.out.println("args.getNonOptionArgs() = " + args.getNonOptionArgs());
+        System.out.println("args.getOptionNames() = " + args.getOptionNames());
+        System.out.println("args.getSourceArgs() = " + Arrays.toString(args.getSourceArgs()));
+
+        for (String optionName : args.getOptionNames()) {
+            System.out.println("args.getOptionValues(optionName) = " + args.getOptionValues(optionName));
+        }
+    }
+}
--- src/main/java/de/diskostu/demo/date4u/Date4uApplication.java
+++ src/main/java/de/diskostu/demo/date4u/Date4uApplication.java
@@ -1,13 +1,29 @@
 package de.diskostu.demo.date4u;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
+@Slf4j
 public class Date4uApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Date4uApplication.class, args);
+    public Date4uApplication() {
+        log.info("constructor");
     }
 
+
+    public static void main(String[] args) {
+        final ConfigurableApplicationContext context = SpringApplication.run(Date4uApplication.class, args);
+
+        final String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            log.info("bean: " + beanDefinitionName);
+        }
+
+        final FileSystem fileSystem = context.getBean(FileSystem.class);
+        System.out.println("freeDiskSpace = " + DataSize.ofBytes(fileSystem.getFreeDiskSpace()).toGigabytes());
+    }
 }
