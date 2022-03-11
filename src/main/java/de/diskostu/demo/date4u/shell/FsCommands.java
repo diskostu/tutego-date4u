45c3cc318e0d543eb375b1348ee097b1e3aa82ca
diskostu <denny.kluge@gmail.com>
2022-03-11 08:31:48 +0100

    added shell component

---
+++ src/main/java/de/diskostu/demo/date4u/shell/FsCommands.java
@@ -0,0 +1,35 @@
+package de.diskostu.demo.date4u.shell;
+
+import de.diskostu.demo.date4u.FileSystem;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.util.unit.DataSize;
+
+@ShellComponent
+public class FsCommands {
+
+    private final FileSystem fileSystem;
+
+
+    public FsCommands(FileSystem fileSystem) {
+        this.fileSystem = fileSystem;
+    }
+
+
+    @ShellMethod("Show application version")
+    public String version() {
+        return "0.1";
+    }
+
+
+    @ShellMethod("add")
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+
+    @ShellMethod("show free disk space in GB")
+    public String freeDiskSpace() {
+        return DataSize.ofBytes(fileSystem.getFreeDiskSpace()).toGigabytes() + " GB";
+    }
+}
